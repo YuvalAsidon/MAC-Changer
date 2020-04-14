37b8a3f8ff51b5351be2725ac0d88f2ae63e62ba
Yuval <yuvalasidon@gmail.com>
2020-04-14 12:47:47 +0300

    Add files via upload

---
+++ MAC_Changer_Linux.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import random
+import re
+
+
+def choose_interface(interface):
+    interface = int(input("Chose out of the {} only one:".format(len(interfaces))))
+    while interface not in range(1, len(interfaces)+1):
+        interface = int(input("Error, chose out of the {} only one:".format(len(interfaces))))
+    return interface
+
+
+def available_nic(interfaces):
+    print("the available NIC interfaces are:")
+    for i in range(len(interfaces)):
+        print("{}: {}".format(i + 1, interfaces[i]))
+
+
+def anser_randmoize():
+    answer = input("Do you want randomized MAC address? (yY/nN) ")
+    while answer not in ["y", "Y", "n", "N"]:
+        answer = input("Incorrect, do you want randomized MAC address? (you can only choose y/Y or n/N) ")
+    return answer
+
+
+def mac_validator():
+    new_mac = input("Please enter the new MAC address (aa:bb:cc:dd:ee:ff) [0-9][A-F]: ")
+    while not re.match("[0-9a-f]{2}(:?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", new_mac.lower()):
+        new_mac = input("Error, please enter the new MAC address (aa:bb:cc:dd:ee:ff) [0-9][A-F]: ")
+    return new_mac
+
+
+interfaces = os.listdir('/sys/class/net/')
+subprocess.call(["sudo", "ifconfig"])
+
+available_nic(interfaces)
+
+interface = choose_interface(interfaces)-1
+print("Change MAC address for {}".format(interfaces[interface]))
+
+subprocess.call(["sudo", "ifconfig", interfaces[interface], "down"])
+
+answer = anser_randmoize()
+
+if answer in ["y", "Y"]:
+    new_mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+else:
+    new_mac = mac_validator()
+
+subprocess.call(["sudo", "ifconfig", interfaces[interface], "hw", "ether", new_mac])
+subprocess.call(["sudo", "ifconfig", interfaces[interface], "up"])
+subprocess.call(["sudo", "ifconfig", interfaces[interface]])
